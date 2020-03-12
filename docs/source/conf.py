abc510d678f0394deaaacf170be6c274420d226a
Helmut Simon <helmut.simon@anu.edu.au>
2020-03-12 20:10:53 +1100

    Use sphinx_rtd_theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,10 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
