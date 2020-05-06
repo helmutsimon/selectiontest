d817b15035de65dc0065b9dd22ce1acc7498d7a7
Helmut Simon <helmut.simon@anu.edu.au>
2020-05-06 12:18:12 +1000

    Update docs and docstrings for cli.

--- cli.py
+++ cli.py
@@ -11,21 +11,7 @@ def selectiontestcli():
 @selectiontestcli.command()
 @click.argument('sfs', nargs=-1)
 def calculate_D(sfs):
-    """
-        Calculate Tajima's D from a site frequency spectrum.
-
-        Parameters
-        ----------
-
-        sfs: list
-            Site frequency spectrum, e.g. [1, 3, 0, 2, 1]
-
-        Returns
-        -------
-        numpy.float64
-            Value of Tajima\'s D.
-
-        """
+    """Calculate Tajima's D from SFS (integers separated by spaces)."""
     sfs = np.asarray(sfs, dtype=float)
     tajd = selectiontest.calculate_D(sfs)
     click.echo(tajd)
@@ -35,26 +21,7 @@ def calculate_D(sfs):
 @click.argument('sfs', nargs=-1)
 @click.option('-r', '--reps', default=10000, help='Number of samples for Monte Carlo integration.')
 def test_neutrality(sfs, reps):
-    """
-    Calculate the log odds ratio of the data for the distribution given by variates0 over
-    the distribution given by variates1.
-
-    Parameters
-    ----------
-
-    sfs: int
-        Site frequency spectrum, e.g. 1 3 0 2 1
-
-    reps: int
-        Number of variates to generate if default is used.
-
-    Returns
-    -------
-
-    numpy.float64
-        Value of log odds ratio. Values can include inf, -inf or nan if one or both probabilities are zero due to underflow error.
-
-    """
+    """Calculate the log odds ratio of the data SFS (integers separated by spaces)."""
     sfs = np.asarray(sfs, dtype=float)
     rho = selectiontest.test_neutrality(sfs, variates0=None, variates1=None, reps=reps)
     click.echo(rho)
@@ -66,29 +33,6 @@ def test_neutrality(sfs, reps):
 @click.option('-r', '--reps', default=10000, help='Number of test samples.')
 @click.option('-f', '--fpr', default=0.02, help='Value of false positive rate set for threshold.')
 def compute_threshold(n, seg_sites, reps, fpr):
-    """
-    Calculate threshold value of :math:`\\rho` corresponding to a given false positive rate (FPR).
-    For values of :math:`\\rho` above the threshold we reject the
-    null (by default neutral) hypothesis.
-
-    Parameters
-    ----------
-    n: int
-        Sample size.
-
-    seg_sites: int
-        Number of segregating sites in sample.
-
-    reps: int
-        Number of variates to generate if default is used.
-
-    fpr: float
-        Selected FPR tolerance.
-
-    Returns
-    -------
-    numpy.float64
-        Threshold value for log odds ratio
-    """
+    """Calculate threshold value of log odds ratio for sample size N and numver of segregating sites SEG_SITES."""
     threshold = selectiontest.compute_threshold(n, seg_sites, reps, fpr)
     click.echo(threshold)
--- docs/source/cli.rst
+++ docs/source/cli.rst
@@ -2,27 +2,17 @@ Command line interface
 ======================
 
 
-The python modules can be run from the command line as follows:
+The following python modules can be run from the bash command line, using the prefix command ``cli'' as shown:
 
 ::
 
-    test_neutrality sfs -q0 variates0 -q1 variates1 -r reps
+    cli test-neutrality sfs -r reps
 
-    calculate_D sfs
+    cli calculate-D sfs
 
-    sample_wf_distribution n outfile -r reps
+    cli compute-threshold n seg_sites -r reps -f fpr
 
-    sample_uniform_distribution n outfile -r reps
+For the command line interface, sfs is entered as a string of integers separated by spaces (variadic argument), e.g. 1 3 0 2 1.
 
-    compute_threshold n seg_sites -r reps -f fpr
-
-For the command line interface, sfs is entered as a string of values separated by commas, e.g. 1,3,0,2,1.
-``sample_wf_distribution`` and ``sample_uniform_distribution`` save output as gzipped pickle files with pathname ``outfile``. These pathnames can be supplied to ``test_neutrality`` by using the options -q0 and -q1.
-
-Note
-----
-
-At present, there is no command line script for the python module piecewise_constant_variates.
-
-
+For more information, use ``cli --help'', ``cli test-neutrality --help'' etc.
 
