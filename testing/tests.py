c422d39c986e7e5cf2a55bda1f5f945e35138f89
Helmut Simon <helmut.simon@anu.edu.au>
2020-05-11 16:41:25 +1000

    Update tests except piecewise and vcf.

--- testing/tests.py
+++ testing/tests.py
@@ -3,26 +3,30 @@
 
 import os
 from cogent3.util.unit_test import TestCase, main
-# from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose
 from cli import selectiontestcli
 from selectiontest import selectiontest
-from selectiontest.selectiontest import calculate_D
-from pandas import read_csv
-from numpy import isclose, sum, array
+from selectiontest.selectiontest import calculate_D, sample_wf_distribution, sample_uniform_distribution
+from selectiontest.selectiontest import test_neutrality, generate_sfs_array, compute_threshold
+from pandas import read_csv, options
+from numpy import isclose, sum, array, all, random, isfinite
 from collections import Counter
 from click.testing import CliRunner
 
 
+__author__ = "Helmut Simon"
+__copyright__ = "Copyright 2020, Helmut Simon"
+__license__ = "GPL"
+__version__ = "0.0.3"
+__email__ = "helmut.simon@anu.edu.au"
+__status__ = "Test"
+
+
 abspath = os.path.abspath(__file__)
 projdir = "/".join(abspath.split("/")[:-1])
-print('projdir=', projdir)
+
 
 from cli import selectiontestcli
-
-#sys.path.insert(0, projdir)
-
-
-
 
 
 def compute_sfs(variant_array):
@@ -31,9 +35,6 @@ def compute_sfs(variant_array):
     sfs = Counter(occurrences)
     sfs = [sfs[i] for i in range(1, n)]
     return array(sfs)
-
-
-print('selectiontest version: ', selectiontest.__version__)
 
 
 class TestTajima(TestCase):
@@ -48,6 +49,7 @@ class TestTajima(TestCase):
     Tajima’s D: -1.446172
     a1=2.828968 a2=1.539768 b1=0.407407 b2=0.279012
     c1=0.053922 c2=0.047227 e1=0.019061 e2=0.004949  """
+    options.mode.chained_assignment = None
 
     def __init__(self):
         # Need to put the data in github
@@ -71,9 +73,129 @@ class TestTajima(TestCase):
         runner = CliRunner()
         result = runner.invoke(selectiontestcli, args=args)
         tajd = float(result.output)
-        assert result.exit_code == 0, str(result.exit_code)
+        assert result.exit_code == 0, "Exit code = %s" % result.exit_code
         assert isclose(-1.446172, tajd, atol=1e-5), 'Incorrect Taj D value (cli)' + str(tajd)
 
 
-TestTajima().test_Tajima_module()
-TestTajima().test_Tajima_cli()
+class Test_wf_distribution(TestCase):
+
+    def __init__(self):
+        self.result = [[0.63994338, 0.23721024, 0.10814627, 0.0147001]
+            , [0.30087787, 0.25618537, 0.22870322, 0.21423354]
+            , [0.65233883, 0.19955567, 0.09403128, 0.05407422]
+            , [0.67011101, 0.12997136, 0.10227359, 0.09764404]
+            , [0.61546645, 0.26632306, 0.07652315, 0.04168734]
+            , [0.45996546, 0.27783446, 0.17732139, 0.0848787]
+            , [0.38108608, 0.22363021, 0.19958542, 0.19569829]
+            , [0.39618915, 0.24045628, 0.19744864, 0.16590593]
+            , [0.60662475, 0.18794874, 0.12206768, 0.08335883]
+            , [0.37366753, 0.24777493, 0.19706648, 0.18149106]]
+
+    def test_sample_wf_distribution(self):
+        x = sample_wf_distribution(5, 10, random_state=7)
+        assert_allclose(x, self.result, err_msg="Failed test_sample_wf_distribution")
+
+
+class Test_uniform_distribution(TestCase):
+
+    def __init__(self):
+        self.result = [[0.85246025, 0.09936954, 0.02485407, 0.02331613],
+           [0.60634303, 0.19092616, 0.130652  , 0.07207881],
+           [0.44714758, 0.27102789, 0.16660389, 0.11522064],
+           [0.49712199, 0.23361777, 0.16482629, 0.10443395],
+           [0.75227604, 0.20319687, 0.0371041 , 0.00742299],
+           [0.48522513, 0.26172446, 0.1626422 , 0.09040821],
+           [0.81234361, 0.15649158, 0.01866754, 0.01249728],
+           [0.8397336 , 0.11600754, 0.03965138, 0.00460748],
+           [0.73013953, 0.13813067, 0.07873048, 0.05299932],
+           [0.46740108, 0.19488643, 0.17406816, 0.16364433]]
+
+    def test_sample_uniform_distribution(self):
+        x = sample_uniform_distribution(5, 10, random_state=11)
+        assert_allclose(x, self.result, err_msg="Failed test_sample_uniform_distribution", atol=1e-5)
+
+
+class Test_test_neutrality(TestCase):
+    def __init__(self):
+        self.sfs = [1, 1, 3, 0, 7, 0]
+        self.result = -1.1955828464862277
+
+    def test_test_neutrality(self):
+        random.seed(3)
+        rho = test_neutrality(self.sfs)
+        assert isclose(rho, self.result), "Failed test of test_neutrality." + str(rho) + str(self.result)
+
+    def test_test_neutrality_cli(self):
+        args = [str(x) for x in self.sfs]
+        args.insert(0, 'test-neutrality')
+        random.seed(3)
+        runner = CliRunner()
+        result = runner.invoke(selectiontestcli, args=args)
+        rho = float(result.output)
+        assert result.exit_code == 0, "Exit code = %s" % result.exit_code
+        assert isclose(rho, self.result), 'Failed test_neutrality (cli)' + str(rho)
+
+
+class Test_compute_threshold(TestCase):
+    def __init__(self):
+        self.n = 5
+        self.seg_sites = 11
+        self.threshold = 0.3894822695822897
+        self.threshold_fpr = 0.4484843725875869
+        self.fpr = 0.05
+        self.sfs_array =    array([[5, 2, 1, 3],
+                                   [5, 1, 2, 3],
+                                   [5, 2, 1, 3],
+                                   [6, 4, 1, 0],
+                                   [6, 1, 1, 3],
+                                   [6, 1, 1, 3],
+                                   [9, 2, 0, 0],
+                                   [6, 5, 0, 0],
+                                   [5, 2, 1, 3],
+                                   [7, 4, 0, 0]])
+
+    def test_generate_sfs_array(self):
+        random.seed = 13
+        x = generate_sfs_array(self.n, self.seg_sites, reps=10, random_state=13)
+        rowsums = x.sum(axis=1)
+        assert all(rowsums == self.seg_sites), "Failed test of compute_threshold (row sums of seg sites)."
+        assert all(x == self.sfs_array), "Failed test of compute_threshold (generate sfs)."
+
+    def test_compute_threshold(self):
+        y = compute_threshold(self.n, self.seg_sites, reps=10, random_state=13)
+        assert isclose(y, self.threshold), "Failed test of compute_threshold." + str(y) + str(self.threshold)
+
+    def test_compute_threshold_fpr(self):
+        y = compute_threshold(self.n, self.seg_sites, reps=10, fpr = self.fpr, random_state=5)
+        assert isclose(y, self.threshold_fpr), "Failed test of compute_threshold." + str(y) + str(self.threshold)
+
+    def test_compute_threshold_cli(self):
+        args = [str(self.n), str(self.seg_sites)]
+        args.insert(0, 'compute-threshold')
+        args.append('-r')
+        args.append(str(10))
+        random.seed = 3
+        runner = CliRunner()
+        result = runner.invoke(selectiontestcli, args=args)
+        assert result.exit_code == 0, "Exit code = %s" % result.exit_code
+        threshold = float(result.output)
+        assert isinstance(threshold, float), "Threshold is not float" + str(threshold)
+        assert isfinite(threshold), "Threshold is not finite" + str(threshold)
+
+
+def main():
+    TestTajima().test_Tajima_module()
+    TestTajima().test_Tajima_cli()
+    Test_wf_distribution().test_sample_wf_distribution()
+    Test_uniform_distribution().test_sample_uniform_distribution()
+    Test_test_neutrality().test_test_neutrality()
+    Test_test_neutrality().test_test_neutrality_cli()
+    Test_compute_threshold().test_generate_sfs_array()
+    Test_compute_threshold().test_compute_threshold()
+    Test_compute_threshold().test_compute_threshold_fpr()
+    Test_compute_threshold().test_compute_threshold_cli()
+    print("All tests for selectiontest version:", selectiontest.__version__,  "complete.")
+
+
+if __name__ == '__main__':
+    main()
