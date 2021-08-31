f71e9b61e727d18a0f13b7d0837773a206e721b3
Helmut Simon <helmut.simon@anu.edu.au>
2021-08-31 20:48:10 +1000

    Update to v0.3.23.

--- selectiontest/__init__.py
+++ selectiontest/__init__.py
@@ -3,7 +3,7 @@
 __author__ = "Helmut Simon"
 __copyright__ = "© Copyright 2020, Helmut Simon"
 __license__ = "BSD-3"
-__version__ = "0.3.22"
+__version__ = "0.3.23"
 __maintainer__ = "Helmut Simon"
 __email__ = "helmut.simon@anu.edu.au"
 __status__ = "Test"
